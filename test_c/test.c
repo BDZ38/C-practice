64fb83d2abac431d94c831cec6b3b79b7400ee65
BDZ38 <bakedbeans@qq.com>
2026-01-15 20:45:29 +0800

    test submiting code by command line

---
+++ test_c/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main ()
+{
+	{
+		int a = 100;
+		
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//extern int a;
+//
+//void test()
+//{
+//	printf("test--->%d\n", a);
+//}
+//
+//int main()
+//{
+//	test();
+//	{
+//		printf("a=%d\n",a);
+//	}
+//	{
+//		
+//		printf("a=%d\n", a);
+//	}
+//	printf("a=%d\n", a);
+//	return 0;
+//}
