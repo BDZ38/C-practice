3304caa27d0c3d242746de128fdff4e1f3a476fd
BDZ38 <bakedbeans@qq.com>
2026-01-20 20:24:54 +0800

    add

--- test_c/源.c
+++ test_c/源.c
@@ -3,22 +3,10 @@
 
 int main()
 {
-	
-	printf("name\tage\tgender\n");
-
-	int i = 0;
-		while (i <= 21)
-		{
-			printf("-");
-			i++;
-		}
-		printf("\nJack\t18 \tMan");
-	
-		
-		
-		
-		char s[] = "oof";
-		printf("\n%s",s);
+	int a = 40;
+	int c = 212;
+	int b = (-8+22)*a-10+c/2;
+	printf("%d\n",b);
 		
 		
 		
