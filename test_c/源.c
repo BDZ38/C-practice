b76e58d91191028dd8761920427919a1246044d4
BDZ38 <bakedbeans@qq.com>
2026-02-10 19:04:50 +0800

    test

--- test_c/源.c
+++ test_c/源.c
@@ -1,17 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <string.h>
-
-int main(){
-	int x,y;
-		scanf("%d", &x);
-		if (x<0)
-			y = -1;
-		else if (x>0)
-			y = 1;
+int sgn(int x) {
+	int z;
+		if (x > 0)
+			z = 1;
+		else if (x < 0) 
+			z = -1;
 		else
-			y = 0;
-		printf("%d\n", y);
+			z = 0;
+	return z;
+}
+int main(){
+	int x;
+		scanf("%d", &x);
+		printf("%d\n",sgn(x));
 		return 0;
 	}
 
