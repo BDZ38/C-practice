d182730a83a24486663fe6602104bf9111c195e9
BDZ38 <bakedbeans@qq.com>
2026-02-19 16:04:33 +0800

    q

--- test_c/源.c
+++ test_c/源.c
@@ -7,8 +7,8 @@ int main() {
 	/*float a = 7 / 2.0;
 	printf("%f\n", a);*/
 	int a = 10;
-	printf("%d\n", size of(a));
-	printf("%d\n", size of(int));
+	printf("%d\n", sizeof a);
+	printf("%d\n", sizeof(int));
 
 		return 0;
 	}
