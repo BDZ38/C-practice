6ed524d1898f83e9faba159fc93f4fddf2f02ee3
BDZ38 <bakedbeans@qq.com>
2026-07-22 22:47:15 +0800

    guess num

--- test_c/practice/source.c
+++ test_c/practice/source.c
@@ -1,47 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
-#include <windows.h>
 #include <stdlib.h>
 #include <time.h>
-void menu()
-{
-	printf("%s\n", "Menu");
-	printf("%s\n", "1.play");
-	printf("%s\n","0.exit");
-}
-void game()
-{
-	srand ((unsigned int)time(NULL));
-	int ret = rand();
-	printf("%d\n",ret);
-}
-int main() {
-	int input = 0;
-	do
-	{
-		menu();
-		printf("%c\n", '>');
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-
-			break;
-		case 0:
-			printf("%s\n","exit game");
-			break;
-		default:
-			printf("again\n");
-			break;
-		}
-	} while (input != 0);
-
-
-
-
-
-}
+//void menu()
+//{
+//	printf("%s\n", "Menu");
+//	printf("%s\n", "1.play");
+//	printf("%s\n","0.exit");
+//}
+//void game()
+//{
+//	int guess = 0;
+//	int ret = rand()%100+1;
+//while(1)
+//	{
+//	printf("%d\n", ret);
+//	printf("guess num>");
+//	scanf("%d", &guess);
+//		if (ret > guess)
+//			{
+//				printf("too small\n");
+//			}
+//		else if (ret < guess)
+//			{
+//				printf("too big\n");
+//			}
+//		else
+//			{
+//				printf("correct\n");
+//				break;
+//			}
+//	}
+//}
+//int main() {
+//	int input = 0;
+//	srand((unsigned int)time(NULL));
+//	do
+//	{
+//		menu();
+//		printf("%c\n", '>');
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//
+//			break;
+//		case 0:
+//			printf("%s\n","exit game");
+//			break;
+//		default:
+//			printf("again\n");
+//			break;
+//		}
+//	} while (input != 0);
+//}
 
 
 
