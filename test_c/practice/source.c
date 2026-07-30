14f194c57aa9f80fdec6d7223c0c6a4e0017401a
BDZ38 <bakedbeans@qq.com>
2026-07-31 00:19:24 +0800

    function

--- test_c/practice/source.c
+++ test_c/practice/source.c
@@ -1,21 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
+#include <string.h>
+
+int findmax(int x, int y)
+{
+	if (x > y)
+		return x;
+	else if (x < y)
+		return y;
+	else
+		return x;
+}
 int main()
 {
-	printf("电脑即将关机，输入pig以取消.\n");
-	system("shutdown -s -t 60");
-	char input[20] = { 0 };
-	do
-	{
-		scanf("%s", input);
-		printf("再试试.\n");
-	}
-	while(strcmp(input, "pig") != 0);
-	system("shutdown -a");
-	printf("关机取消.\n");
+	int x;
+	int y;
+	scanf("%d %d", & x,& y);
+	int z = findmax(x, y);
+	printf("%d\n", z);
 	return 0;
 }
